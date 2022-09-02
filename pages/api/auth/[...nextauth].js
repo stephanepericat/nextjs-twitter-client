e1722d58290affe30a0c5fd0675781610db40615
Stephane P. Pericat <shortwavaudio@gmail.com>
2022-09-02 11:21:10 -0500

    feat: next auth integration

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth"
+import TwitterProvider from "next-auth/providers/twitter"
+
+export default NextAuth({ 
+  providers: [
+    TwitterProvider({
+      clientId: process.env.TWITTER_ID,
+      clientSecret: process.env.TWITTER_SECRET,
+      version: "2.0"
+    })
+  ],
+  callbacks: {
+    // async session({ session, token, user }) {
+    //   return session
+    // }
+  }
+})
