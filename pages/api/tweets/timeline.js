c1c7421a9c7a0fb7fe1ece770506bbc2226a11da
Stephane P. Pericat <shortwavaudio@gmail.com>
2022-09-04 16:45:57 -0500

    chore: timeline fixture

--- pages/api/tweets/timeline.js
+++ pages/api/tweets/timeline.js
@@ -13,10 +13,74 @@ export default async (req, res) => {
     const URI = TIMELINE_REVERSE_CHRONOLOGICAL.replace(":id", userid);
     const { client } = useAxios(accessToken);
 
+    const expansions = [
+      "attachments.media_keys",
+      "attachments.poll_ids",
+      "author_id",
+      "geo.place_id",
+      "referenced_tweets.id",
+    ];
+
+    const mediaFields = [
+      "alt_text",
+      "duration_ms",
+      "height",
+      "media_key",
+      "preview_image_url",
+      "type",
+      "url",
+      "width",
+    ];
+
+    const placeFields = [
+      "contained_within",
+      "country",
+      "country_code",
+      "full_name",
+      "geo",
+      "id",
+      "name",
+      "place_type",
+    ];
+
+    const pollFields = [
+      "duration_minutes",
+      "end_datetime",
+      "id",
+      "options",
+      "voting_status",
+    ];
+
+    const tweetFields = [
+      "created_at",
+      "entities",
+      "geo",
+      "possibly_sensitive",
+      "public_metrics",
+      "referenced_tweets",
+      "reply_settings",
+      "source",
+      "withheld",
+    ];
+
+    const userField = [
+      "profile_image_url",
+      "protected",
+      "url",
+      "verified",
+      "withheld",
+    ];
+
     try {
       const { data } = await client.get(URI, {
         params: {
-          expansions: "author_id",
+          expansions: expansions.join(),
+          "media.fields": mediaFields.join(),
+          // TODO: user later...
+          // "place.fields": placeFields.join(),
+          // "poll.fields": pollFields.join(),
+          "tweet.fields": tweetFields.join(),
+          "user.fields": userField.join(),
         },
       });
 
